166038749b896612dee50ca6ff41c9a88e318869
nedols <vnatale83@gmail.com>
2026-02-26 11:58:25 +0100

    Update package definition

--- Package.swift
+++ Package.swift
@@ -1,17 +1,21 @@
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
    name: "Shared",
    platforms: [
-     .iOS(.v14),
+     .iOS(.v16),
    ],
    products: [
-      .library(name: "Shared", targets: ["Shared"])
+      .library(
+         name: "Shared", 
+         targets: ["Shared"]
+      )
    ],
    targets: [
       .binaryTarget(
          name: "Shared",
-         url: "https://github.com/nedols/kotlin-multiplatform-fibonacci/releases/download/1.0.0/Shared.xcframework.zip",
+         url: "https://github.com/nedols/kotlin-multiplatform-fibonacci/releases/download/1.0.1/Shared.xcframework.zip",
          checksum:"6eeb20fc23f3f8ea82d91329a8b6bb16210458355cdb640f430006ea006f6929")
    ]
 )
